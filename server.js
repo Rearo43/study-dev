cffc92e16410a5a1b09e2fcc053978c807962ee0
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2021-09-28 22:00:51 -0700

    working on grabbing info from api

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3005;
 
 const express = require('express');
-// const superagent = require('superagent');
+const superagent = require('superagent');
 // const pg = require('pg');
 const morgan = require('morgan');
+
+// const client = new pg.Client(process.env.)
 
 const app = express();
 
@@ -17,12 +19,43 @@ app.use(express.static('./public'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', home);
+app.get('/', movies);
 app.use('*', routeNotFound);
 app.use(bigError);
 
-function home(req, resp) {
-  resp.status(200).render('pages/index');
+// function home(req, res) {
+//   res.status(200).render('pages/index');
+// }
+
+function movies(req, resp) {
+  const API = 'https://api.themoviedb.org/3/search/movie';
+
+  let qObject = {
+    api_key: process.env.MOVIES,
+    query: req.query.search_query,
+  };
+
+  superagent
+    .get(API)
+    .query(qObject)
+    .then((getMovies) => {
+      let moviesArr = getMovies.body.results.map((movie) => {
+        return new Movies(movie);
+      });
+
+      resp.status(200).json(moviesArr);
+    })
+    .catch(() => resp.status(500).send('Movies Broken!'));
+}
+
+function Movies(info) {
+  this.title = info.original_title;
+  this.overview = info.overview;
+  this.average_votes = info.vote_average;
+  this.total_votes = info.vote_count;
+  this.image_url = `https://image.tmdb.org/t/p/w500${info.poster_path}`;
+  this.popularity = info.popularity;
+  this.released_on = info.release_date;
 }
 
 //----------404 Error
